2801d458ced3445f4632c8b914eecbbb61e43a73
João <jp6nunes@gmail.com>
2020-04-13 16:27:59 -0300

    Correção Animação View

--- js/animacoes.js
+++ js/animacoes.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 	var $target = $('.anime'),
 			animationClass = 'anime-init',
 			windowHeight = $(window).height(),
-			offset = windowHeight - (windowHeight / 6);
+			offset = windowHeight - (windowHeight /10);
 
 	function animeScroll() {
 		var documentTop = $(document).scrollTop();
