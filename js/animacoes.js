bba9b9de85b93c308ff74e862d08fe4859241878
João <jp6nunes@gmail.com>
2020-04-13 16:57:48 -0300

    Correção animation view

--- js/animacoes.js
+++ js/animacoes.js
@@ -1,16 +1,23 @@
 var root = document.documentElement;
 root.className += ' js';
 
+
+
+ 
 function boxTop(idBox) {
 	var boxOffset = $(idBox).offset().top;
 	return boxOffset;
 }
-
 $(document).ready(function() {
 	var $target = $('.anime'),
 			animationClass = 'anime-init',
 			windowHeight = $(window).height(),
-			offset = windowHeight - (windowHeight /10);
+			offset = windowHeight - (windowHeight/10);
+	document.body.onresize = function() {
+				windowHeight = $(window).height();
+				offset = windowHeight - (windowHeight/10);
+			 };
+			 
 
 	function animeScroll() {
 		var documentTop = $(document).scrollTop();
